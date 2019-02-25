3214a770b9a995ee3a2cf9ed7845eddf9aa09667
shyamsarnaik <shyam.sarnaik@gmail.com>
2019-02-25 21:31:15 +0530

    WIP

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-app'`, () => {
+  it(`should have as title 'angular7-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-app');
+    expect(app.title).toEqual('angular7-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular7-app!');
   });
 });
